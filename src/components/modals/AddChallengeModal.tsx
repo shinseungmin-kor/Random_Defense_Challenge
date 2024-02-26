ed76dcdbfdf7d09b31a1df2e8847944b023d4beb
신승민 <shinseungmin10@gmail.com>
2024-02-27 00:10:44 +0900

    모달 백그라운드

--- src/components/modals/AddChallengeModal.tsx
+++ src/components/modals/AddChallengeModal.tsx
@@ -1,23 +1,58 @@
-import { View, Text, Modal, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  Modal,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 import React, { useState } from 'react';
 import Title from '../text/Typography';
 import FontStyle from '../../statics/styles/Font';
+import { Color } from '../../statics/styles/Color';
 
 interface IProps {
   toggleModal: () => void;
 }
+const screenWidth = Math.round(Dimensions.get('window').width);
 
 const AddChallengeModal = (props: IProps) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View style={{ backgroundColor: 'white', padding: 20 }}>
+    <View style={styles.container}>
+      <View style={styles.modal}>
         <Text>Modal is open!</Text>
         <TouchableOpacity onPress={props.toggleModal}>
           <Text>Close Modal</Text>
         </TouchableOpacity>
       </View>
+      <View style={styles.background}></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  background: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: Color.Gray100,
+    opacity: 0.8,
+    position: 'absolute',
+    zIndex: 1,
+  },
+  modal: {
+    width: screenWidth,
+    height: 500,
+    backgroundColor: Color.White,
+    padding: 20,
+    position: 'absolute',
+    zIndex: 2,
+  },
+});
 
 export default AddChallengeModal;
